04fd2615ad7370d727c03cb2aa40fc4380eef7b4
Simi Akinrele <akinrelesimisola@yahoo.com>
2025-07-31 19:07:26 -0400

    update validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :vendor_id, presence: true
   validates :sku, presence: true, uniqueness: true
-  
+  validates :sku, format: { with: /\A[A-Z0-9]+(?:-[A-Z0-9]+)*\z/, message: "must be uppercase letters and numbers with optional hyphens" }
+
   belongs_to :vendor
 end
